a5fee5cc38db39d3ca5c759bdfcd30d55156b912
suparansharma <suparansharmap@gmail.com>
2023-03-18 00:15:59 +0600

    persional portfolio template

---
+++ admin_type/Nazox_React_v2.0.0/Starterkit/src/pages/Dashboard/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { Container, Row, Col } from "reactstrap";
+
+//Import Breadcrumb
+import Breadcrumbs from '../../components/Common/Breadcrumb';
+
+class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            breadcrumbItems : [
+                { title : "Nazox", link : "/" },
+                { title : "Dashboard", link : "#" },
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className="page-content">
+                    <Container fluid>
+
+                    <Breadcrumbs title="Dashboard" breadcrumbItems={this.state.breadcrumbItems} />
+
+                    </Container> 
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Dashboard;
---
+++ admin_type/Nazox_React_v2.0.0/Starterkit/src/routes/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+
+// Authentication related pages
+import Login from "../pages/Authentication/Login";
+import Logout from "../pages/Authentication/Logout";
+import Register from "../pages/Authentication/Register";
+import ForgetPwd from "../pages/Authentication/ForgetPassword";
+import AuthLockScreen from "../pages/Authentication/AuthLockScreen";
+
+// Dashboard
+import Dashboard from "../pages/Dashboard/index";
+
+// Inner Authentication
+import Login1 from "../pages/AuthenticationInner/Login";
+import Register1 from "../pages/AuthenticationInner/Register";
+import ForgetPwd1 from "../pages/AuthenticationInner/ForgetPassword";
+
+const authProtectedRoutes = [
+
+	{ path: "/dashboard", component: Dashboard },
+
+	// this route should be at the end of all other routes
+	{ path: "/", exact: true, component: () => <Redirect to="/dashboard" /> }
+];
+
+const publicRoutes = [
+	{ path: "/logout", component: Logout },
+	{ path: "/login", component: Login },
+	{ path: "/forgot-password", component: ForgetPwd },
+	{ path: "/register", component: Register },
+	{ path: "/lock-screen", component: AuthLockScreen },
+
+	// Authentication Inner
+	{ path: "/auth-login", component: Login1 },
+	{ path: "/auth-register", component: Register1 },
+	{ path: "/auth-recoverpw", component: ForgetPwd1 },
+
+];
+
+export { authProtectedRoutes, publicRoutes };
